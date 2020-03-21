8090ed1d8aff00b98b9f0e737a88214f9fc1d2e9
jesulonimi21 <anjola4jeez@gmail.com>
2020-03-21 20:13:42 +0100

    debugging

--- js/index.js
+++ js/index.js
@@ -18,56 +18,52 @@ let isOpen=false;
         document.getElementById("viewPatients").addEventListener("click",toggleNavBar);
         let contractInstance=null;
         let client=null;
-        let contractAddress="ct_29T5fodtK95iDLiJsNTmwVb1F8eVp2P6PE59Z4EYHhY8zjLLUh";
+        let contractAddress="ct_26nkg4ncfetry1nUZwBN2dPViFNLLHGZkMq4t2w3PEKWXsxpAn";
         let contractSource=`
-        contract HospitalManagementSystemContract=
-            record patientsPersonalInfo={
-                nameOfPatient:string,
-                age:string,
-                sex:string,
-                dateOfAdmission:string,
-                patientID:string,
-                patientHealthCondition:string,
-                patientHomeAddress:string,
-                patientsPhoneNumber:string,
-                nextofkin:string,
-                nextofkinPhoneNumber:string,
-                ipfsHash:string 
-                }
-            record patientsMedicalInfo={
-                height:string,
-                weight:string,
-                bmi:string,
-                genotype:string,
-                bloodgroup:string,
-                patientsMedicalHistory:string,
-                medicationGiven:string
-                }
-            record state={
-                recordManager: map(address,list(patientsPersonalInfo)),
-                patientsMedicalrecord: map(address,list(patientsMedicalInfo))
-                }
-            stateful entrypoint init()={recordManager={},patientsMedicalrecord={}}
-
-            stateful entrypoint registerpatientsPersonalInfo(nameOfPatient':string,age':string,sex':string,dateOfAdmission':string,patientID':string,patientHealthCondition':string,patientHomeAddress':string,patientsPhoneNumber':string,nextofkin':string,nextofkinPhoneNumber':string,ipfsHash':string)=
-                let recordManagerListofPatients=Map.lookup_default(Call.caller,state.recordManager,[])
-                let newPatientInfo={nameOfPatient=nameOfPatient',age=age',sex=sex',dateOfAdmission=dateOfAdmission',patientID=patientID',patientHealthCondition=patientHealthCondition',patientHomeAddress=patientHomeAddress',patientsPhoneNumber=patientsPhoneNumber',nextofkin=nextofkin', nextofkinPhoneNumber=nextofkinPhoneNumber',ipfsHash=ipfsHash'}
-                let newlistofPatients=newPatientInfo::recordManagerListofPatients
-                put(state{recordManager[Call.caller]=newlistofPatients})
-
-
-            stateful entrypoint registerpatientsMedicalInfo(height':string,weight':string,bmi':string,genotype':string,bloodgroup':string,patientsMedicalHistory':string,medicationGiven':string)=
-                let patientsMedicalrecordListofPatients=Map.lookup_default(Call.caller,state.patientsMedicalrecord,[])
-                let newPatientMedicalInfo={height=height',weight=weight',bmi=bmi',genotype=genotype',bloodgroup=bloodgroup',patientsMedicalHistory=patientsMedicalHistory',medicationGiven=medicationGiven'}
-                let newlistofPatientsMedicalInfo=newPatientMedicalInfo::patientsMedicalrecordListofPatients
-                put(state{patientsMedicalrecord[Call.caller]=newlistofPatientsMedicalInfo})
-
-            entrypoint getrecordManagerListofPatients()=
-                state.recordManager[Call.caller]   
-
-            entrypoint getpatientsMedicalrecordListofPatients()=
-                state.patientsMedicalrecord[Call.caller]
-
+    contract HospitalManagementSystemContract=
+        record patientsPersonalInfo={
+            nameOfPatient:string,
+            age:string,
+            sex:string,
+            dateOfAdmission:string,
+            patientID:string,
+            patientHealthCondition:string,
+            patientHomeAddress:string,
+            patientsPhoneNumber:string,
+            nextofkin:string,
+            nextofkinPhoneNumber:string,
+            ipfsHash:string 
+            }
+        record patientsMedicalInfo={
+            height:string,
+            weight:string,
+            bmi:string,
+            genotype:string,
+            bloodgroup:string,
+            patientsMedicalHistory:string,
+            medicationGiven:string
+            }
+        record state={
+            recordManager: map(address,list(patientsPersonalInfo)),
+            patientsMedicalrecord: map(address,list(patientsMedicalInfo))
+            }
+        stateful entrypoint init()={recordManager={},patientsMedicalrecord={}}
+        stateful entrypoint registerpatientsPersonalInfo(nameOfPatient':string,age':string,sex':string,dateOfAdmission':string,patientID':string,patientHealthCondition':string,patientHomeAddress':string,patientsPhoneNumber':string,nextofkin':string,nextofkinPhoneNumber':string,ipfsHash':string)=
+            let recordManagerListofPatients=Map.lookup_default(Call.caller,state.recordManager,[])
+            let newPatientInfo={nameOfPatient=nameOfPatient',age=age',sex=sex',dateOfAdmission=dateOfAdmission',patientID=patientID',patientHealthCondition=patientHealthCondition',patientHomeAddress=patientHomeAddress',patientsPhoneNumber=patientsPhoneNumber',nextofkin=nextofkin', nextofkinPhoneNumber=nextofkinPhoneNumber',ipfsHash=ipfsHash'}
+            let newlistofPatients=newPatientInfo::recordManagerListofPatients
+            put(state{recordManager[Call.caller]=newlistofPatients})
+        stateful entrypoint registerpatientsMedicalInfo(height':string,weight':string,bmi':string,genotype':string,bloodgroup':string,patientsMedicalHistory':string,medicationGiven':string)=
+            let patientsMedicalrecordListofPatients=Map.lookup_default(Call.caller,state.patientsMedicalrecord,[])
+            let newPatientMedicalInfo={height=height',weight=weight',bmi=bmi',genotype=genotype',bloodgroup=bloodgroup',patientsMedicalHistory=patientsMedicalHistory',medicationGiven=medicationGiven'}
+            let newlistofPatientsMedicalInfo=newPatientMedicalInfo::patientsMedicalrecordListofPatients
+            put(state{patientsMedicalrecord[Call.caller]=newlistofPatientsMedicalInfo})
+        entrypoint getrecordManagerListofPatients()=
+            Map.lookup_default(Call.caller,state.recordManager,[])
+              
+        entrypoint getpatientsMedicalrecordListofPatients()=
+            Map.lookup_default(Call.caller,state.patientsMedicalrecord,[])
+            
     `;
     let patientImage="null";
     let ipfs="null";
